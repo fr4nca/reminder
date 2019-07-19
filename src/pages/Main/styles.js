f8598c16223e15955ccd5010f1c607839c879bbc
Victor Almeida França <fr4ncacc@gmail.com>
2019-07-18 21:03:54 -0300

    first setup

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Text = styled.Text`
+  font-size: 22px;
+`;
+
+export const View = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
